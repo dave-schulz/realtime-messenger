8f9151ebd80ecd4d01de1c495ce7fbcc1d89c3ad
Davey Schulz <daveschulz@hotmail.nl>
2023-05-07 18:12:14 +0200

    Finished up styling the sidebar and show the current logged in user

--- app/components/Sidebar/DesktopSidebar.tsx
+++ app/components/Sidebar/DesktopSidebar.tsx
@@ -4,9 +4,18 @@ import useRoutes from '@/app/hooks/useRoutes';
 import { FC, useState } from 'react';
 import DesktopItem from './DesktopItem';
 
-const DesktopSidebar: FC = ({}) => {
+import { User } from '@prisma/client';
+import Avatar from './Avatar';
+
+interface DesktopSidebarProps {
+  currentUser: User;
+}
+
+const DesktopSidebar: FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  console.log(currentUser);
 
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
@@ -23,6 +32,14 @@ const DesktopSidebar: FC = ({}) => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
