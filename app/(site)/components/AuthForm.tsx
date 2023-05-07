cf1009524187aefbfd985a1fda43b5f968f2910b
Davey Schulz <daveschulz@hotmail.nl>
2023-05-07 11:12:36 +0200

    Added the register functionality

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { FC, useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -37,9 +38,11 @@ const AuthForm: FC = ({}) => {
     setIsLoading(true);
 
     if (variant === 'REGISTER') {
+      axios.post('/api/register', data);
     }
 
     if (variant === 'LOGIN') {
+      // axios.put('');
     }
   };
 
